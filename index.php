557c9421eaa722257165f135a77afb510fa131e8
Benjamin Sanchez <Linkandcheezer@gmail.com>
2019-03-15 23:14:01 -0700

    -Added the first 2 section of the webpage -Added css

--- index.php
+++ index.php
@@ -3,11 +3,67 @@
 <html>
   <head>
     <title>Quality-Backhoe-Services</title>
+    <link type='text/css' rel='stylesheet' href="./style.css">
   </head>
 
   <body>
 
-    
+    <!-- Navigation -->
+    <nav id='nav-bar'>
+
+      <img src="./images/logo" alt="small logo"/>
+      <div class="social-media-icons">
+        <img src="./images/facebook" alt="facebook logo">
+        <img src="./images/twitter" alt="twitter logo">
+        <img src="./images/instagram" alt="instagram logo">
+      </div>
+
+      <p>OFFICE: 509.545.0242</p>
+
+      <li id='navigation-buttons'>
+        <ul class='nav-button'>Home</ul>
+        <ul class='nav-button'>Services</ul>
+        <ul class='nav-button'>About</ul>
+        <ul class='nav-button'>Contact</ul>
+      </li>
+
+    </nav>
+    <!-- Navigation -->
+
+    <!-- Background Image and Super Logo -->
+    <header>
+      <img src='' alt="Header image of a construction truck sitting dormant
+      during a sunset while workers talk amongst themselves">
+
+      <img src='' alt='giant logo of QualityBackhoeServices'>
+    </header>
+    <!-- Background Image and Super Logo -->
+
+    <!-- Welcome Wrapper [outside flow] -->
+    <div id='welcome-div' class='floating-box'>
+      <h1>PROVIDING QUALITY CRAFTSMANSHIP</h1>
+      <p>General contractor since 2001 serving the Tri-Cities area with all phases of excavation!</p>
+    </div>
+    <!-- Welcome Wrapper [outside flow] -->
+
+    <!-- Welcome div -->
+    <section>
+      <img src='' alt='background-image of welder'/>
+      <img src='' alt='workers measuring tubing'/>
+      <div>
+        <h3>Call for a FREE estimate!</h3>
+        <br/>
+
+        <img src='' alt='telephone icon'/>
+        <p>OFFICE: 509.545.0242</p>
+
+        <br/>
+        <img src='' alt='cellphone icon'/>
+        <p>MOBILE: 509.727.5112</p>
+      </div>
+    </section>
+    <!-- Welcome div -->
+
 
   </body>
 
